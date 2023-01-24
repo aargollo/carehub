32a7884a3fed4c52e197e36522cf31dc37889393
alexanderargollo <alexanderargollo@MacBook-Pro-de-alexanderargollo.local>
2023-01-24 15:40:44 -0300

    Cleannig up react app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "carehubb23af7b4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+// referencia https://aws.amazon.com/pt/blogs/mobile/amplify-uis-new-authenticator-component-makes-it-easy-to-add-customizable-login-pages-to-your-react-angular-or-vue-app/
+
 import './App.css';
-function App() {
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+// novo codigo from aws page
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+import React from 'react';
+Amplify.configure(awsExports);
+
+//import {AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+Amplify.configure(awsConfig);
+
+// nova funcao do codigo from aws page
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <h1> Welcome to CareBOX</h1>
-    </div>
+    <div>
+      <h1> Olá {user.username}</h1>
+      <h1> Bem vindo ao CareBOX!</h1>
+      <button onClick={signOut}>Sign out</button>
+      </div>
   );
 }
 
-export default App;
+//export default App;
+export default withAuthenticator(App);
